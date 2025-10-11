5fd3f1ec17b5fbd2113a03296d39f365f05e14dc
Anthonny <tonysolorzano880@gmail.com>
2025-10-10 21:56:07 -0500

    feat(bloc): implementacion basica de bloc_notifications
    Se agregaron nuevas dependecias y se modifico el main para permitir el
    acceso al estado del bloc en toda la aplicacion.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notification_app/config/router/app_router.dart';
 import 'package:notification_app/config/theme/app_theme.dart';
+import 'package:notification_app/presentation/blocs/notifications/notifications_bloc.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => NotificationsBloc())],
+      child: MainApp(),
+    ),
+  );
+  // const MainApp());
 }
 
 class MainApp extends StatelessWidget {
